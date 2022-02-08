ff3c1a4f4f62432d08a82dafe55636181132e44e
Vannoch Sodex <sdxpm.pro@gmail.com>
2022-02-08 10:13:10 +0700

    create calculator package

---
+++ src/Calculator.php
@@ -0,0 +1,12 @@
+<?php 
+    class Calculator{
+        function __construct(){
+
+        }
+        function sumFunction($para1,$para2){
+            return $para1 + $para2;
+        }
+    }
+
+
+?>
---
+++ src/SdxServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+namespace Sdx\CalculatorPackage;
+use Illuminate\Support\ServiceProvider;
+class SdxServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+    public function register()
+    {
+        
+    }
+}
